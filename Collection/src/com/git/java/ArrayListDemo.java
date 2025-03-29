78580820b07ed35c5f28f3c2f6bc1ada8ec3a539
Snehal <snehalrote20@gmail.com>
2025-03-29 21:42:41 +0530

    Arraylist code added for city

---
+++ Collection/src/com/git/java/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package com.git.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		List<String> list=new ArrayList<String>();
+		list.add("Pune");
+		list.add("Jalna");
+		list.add("Mumbai");
+		list.add("Delhi");
+		list.add("Akola");
+		System.out.println(list);
+	}
+
+}
