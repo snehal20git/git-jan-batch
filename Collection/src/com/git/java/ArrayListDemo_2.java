827d33562e64b01e9ddc1626ef4033883edbf852
Snehal <snehalrote20@gmail.com>
2025-03-29 21:47:59 +0530

    Arraylist2 code added for integer

---
+++ Collection/src/com/git/java/ArrayListDemo_2.java
@@ -0,0 +1,16 @@
+package com.git.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo_2 {
+	public static void main(String[] args) {
+		List<Integer> list=new ArrayList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		System.out.println(list);
+		
+	}
+
+}
