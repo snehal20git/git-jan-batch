e7b37a663274ed5261d233d4f773c9fe9d1843ae
Snehal <snehalrote20@gmail.com>
2025-03-29 23:17:13 +0530

    List Code is added for String names

---
+++ Collection_1/src/com/git/example/List.java
@@ -0,0 +1,15 @@
+package com.git.example;
+
+import java.util.ArrayList;
+
+public class List {
+
+	public static void main(String[] args) {
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("Snehal");
+		list.add("soham");
+		System.out.println(list);
+		
+		
+	}
+}
